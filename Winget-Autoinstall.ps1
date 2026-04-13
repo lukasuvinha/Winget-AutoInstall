8438dbde3d3a0cb8a5fbfc9877c2b9b088695da6
lukasuvinha <47397238+lukasuvinha@users.noreply.github.com>
2026-04-13 10:51:21 -0300

    Update Winget-Autoinstall.ps1

--- Winget-Autoinstall.ps1
+++ Winget-Autoinstall.ps1
@@ -57,19 +57,7 @@ ping -n 3 https://drivers.certisign.com.br
 
 # Baixar os arquivos
 #Invoke-WebRequest -Uri "https://github.com/thedoggybrad/Paint3DArchive/releases/download/b2/2024.2410.13017.0.AppxBundle" -OutFile "$destination\Paint3D.AppxBundle"
-Invoke-WebRequest -Uri "https://download.microsoft.com/download/4/5/9/4590E1BD-CDA7-41F1-86A1-CB8005DDDF96/PTB/SSCERuntime_x64-PTB.exe" -OutFile "$destination\SSCERuntime_x64-PTB.exe"
-Invoke-WebRequest -Uri "https://acessorias.s3.us-east-2.amazonaws.com/eContinuoSetup/eContinuo(w10x64).exe" -OutFile "$destination\eContinuo_w10x64.exe"
-Invoke-WebRequest -Uri "https://www.dominioweb.com.br/pluginDownload/dominioweb_plugin_setup.exe" -OutFile "$destination\dominioweb_plugin_setup.exe"
-Invoke-WebRequest -Uri "https://www.serpro.gov.br/links-fixos-superiores/assinador-digital/assinador-serpro/arquivos/AssinadorSERPRO4.2.1.exe" -OutFile "$destination\AssinadorSERPRO4.2.1.exe"
-Invoke-WebRequest -Uri "https://drivers.certisign.com.br/midias/leitoras/gemalto/gempctwin-usb/64-bits/gemccid_en-us_64.msi" -OutFile "$destination\gemccid_en-us_64.msi"
-Invoke-WebRequest -Uri "https://drivers.certisign.com.br/midias/leitoras/scm/scr3310/SCR3_DriversOnly_V8.41.exe" -OutFile "$destination\SCR3_DriversOnly_V8.41.exe"
-Invoke-WebRequest -Uri "https://drivers.certisign.com.br/midias/gerenciadores/safesign/64bits/SafeSignIC30124-x64-win-tu-admin.exe" -OutFile "$destination\SafeSignIC30124-x64-win-tu-admin.exe"
-Invoke-WebRequest -Uri "https://drivers.certisign.com.br/midias/tokens/safenet/64bits/certisign10.6-x64-10.6.exe" -OutFile "$destination\certisign10.6-x64-10.6.exe"
-Invoke-WebRequest -Uri "https://drivers.certisign.com.br/midias/leitoras/oberthur/64-bits/AWP_Manager_5.1.8_64_bits.exe" -OutFile "$destination\AWP_Manager_5.1.8_64_bits.exe"
-Invoke-WebRequest -Uri "https://drivers.certisign.com.br/midias/tokens/gdburti/64bits/2k-xp-vi-7/GDsetupStarsignCUTx64.exe" -OutFile "$destination\GDsetupStarsignCUTx64.exe"
-Invoke-WebRequest -Uri "https://drivers.certisign.com.br/midias/gerenciadores/safesign/64bits/SafeSignIC30124-x64-win-tu-admin.exe" -OutFile "$destination\SafeSignIC30124-x64-win-tu-admin.exe"
-Invoke-WebRequest -Uri "https://drivers.certisign.com.br/midias/tokens/safenet/32bits/certisign10.6-x32-10.6.exe" -OutFile "$destination\certisign10.6-x32-10.6.exe"
-Invoke-WebRequest -Uri "https://www.caixa.gov.br/Downloads/FGTS-token-conectividade/Instalador_Kriptonita_1_0_6.zip" -OutFile "$destination\Instalador_Kriptonita_1_0_6.zip"
+#Invoke-WebRequest -Uri "https://download.microsoft.com/download/4/5/9/4590E1BD-CDA7-41F1-86A1-CB8005DDDF96/PTB/SSCERuntime_x64-PTB.exe" -OutFile "$destination\SSCERuntime_x64-PTB.exe"
 
 ping -n 3 https://github.com/SasaDermanovic/2024-Winget-Setup-Guide-Windows-10-LTSC
 #Invoke-WebRequest -Uri "https://github.com/SasaDermanovic/2024-Winget-Setup-Guide-Windows-10-LTSC/archive/refs/heads/main.zip" -OutFile "$destination\"
